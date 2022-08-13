dabafb50eff3cf4dde96c3e10916d19038e56353
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2022-08-12 23:23:31 -0400

    Add in the port detection

--- Linux.py
+++ Linux.py
@@ -1,54 +1,108 @@
-from pathlib import Path
+import re
 import subprocess
-from Scripts import iokit, shared
+from operator import itemgetter
+from pathlib import Path
+
 from base import BaseUSBMap
+from Scripts import shared
 
 
 def quick_read(path: Path):
-    return path.read_text().strip()
+    return path.read_text().strip("\x00").strip()
 
 
 def quick_read_2(path: Path, name: str):
-    return quick_read(path / Path(name))
+    return quick_read(path / name)
 
 
-controller_paths: list[Path] = []
+controller_paths: set[Path] = set()
 
 
-for bus_path in Path("/sys/bus/usb/devices").iterdir():
+for bus_path in Path("../utb-samples/gwy/sys/bus/usb/devices").iterdir():
     # Only look at buses
     if not bus_path.stem.startswith("usb"):
         continue
 
     # The parent of the bus is the controller
-    controller_paths.append(bus_path.resolve().parent)
+    controller_paths.add(bus_path.resolve().parent)
 
 for controller_path in controller_paths:
     print(f"Processing controller {controller_path}")
 
-    lspci_output = subprocess.run(["lspci", "-vvvmm", "-s", controller_path.stem], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
-    lspci_output = {i.partition(":\t")[0]: i.partition("\t")[2] for i in lspci_output.splitlines() if i}
+    # lspci_output = subprocess.run(["lspci", "-vvvmm", "-s", controller_path.stem], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+    # lspci_output = {i.partition(":\t")[0]: i.partition("\t")[2] for i in lspci_output.splitlines() if i}
 
     controller = {
-        "name": lspci_output["Device"],
+        # "name": lspci_output["Device"],
         "identifiers": {
             "bdf": [int(i, 16) for i in [controller_path.name[5:7], controller_path.name[8:10], controller_path.suffix[1]]],
             "pci_id": [quick_read_2(controller_path, "vendor")[2:], quick_read_2(controller_path, "device")[2:]],
         },
-        "ports": []
+        "ports": [],
     }
 
-    if (controller_path / Path("revision")).exists():
-        controller["identifiers"]["pci_revision"] = int(quick_read(controller_path / Path("revision")), 16)
+    if (controller_path / "revision").exists():
+        controller["identifiers"]["pci_revision"] = int(quick_read(controller_path / "revision"), 16)
 
-    if (controller_path / Path("subsystem_vendor")).exists() and (controller_path / Path("subsystem_device")).exists():
+    if (controller_path / "subsystem_vendor").exists() and (controller_path / "subsystem_device").exists():
         controller["identifiers"]["pci_id"] += [quick_read_2(controller_path, "subsystem_vendor")[2:], quick_read_2(controller_path, "subsystem_device")[2:]]
 
-    if (controller_path / Path("firmware_node/path")).exists():
+    if (controller_path / "firmware_node/path").exists():
         controller["identifiers"]["acpi_path"] = quick_read_2(controller_path, "firmware_node/path")
 
-    controller["class"] = shared.USBControllerTypes(int(quick_read(controller_path / Path("class")), 16) & 0xff)
+    controller["class"] = shared.USBControllerTypes(int(quick_read_2(controller_path, "class"), 16) & 0xFF)
 
     # Enumerate the buses
     for hub in controller_path.glob("usb*"):
-        
+        # maxchild, speed, version
+        bus_number = quick_read_2(hub, "busnum")
+        hub_info = {
+            "hub_name": hub.name,
+            "port_count": int(quick_read_2(hub, "maxchild")),
+            "speed": shared.USBDeviceSpeeds.from_speed(int(quick_read_2(hub, "speed"))),
+            "version": quick_read_2(hub, "version"),
+            "ports": [],
+        }
+        print(hub_info)
+        # Get the ports
+        ports = hub.glob(f"{bus_number}-0:1.0/usb{bus_number}-port*")
+        for port in sorted(ports, key=lambda x: int(x.name.replace(f"usb{bus_number}-port", ""))):
+            port_info = {
+                "name": quick_read_2(port, "firmware_node/path").split(".")[-1],
+                "comment": None,
+                "index": int(port.name.replace(f"usb{bus_number}-port", "")),  # Need to parse it from the name. I hate linux
+                "class": hub_info["speed"],  # tbd
+                "type": None,
+                "guessed": None,  # tbd
+                "connect_type": quick_read_2(port, "connect_type"),
+                "devices": [],
+                "type_c": False,
+            }
+
+            if (port / "peer").exists():
+                port_info["companion_info"] = re.match(r"(?P<hub>usb\d+)-port(?P<port>\d+)", (port / "peer").resolve().name).groupdict()
+            else:
+                port_info["companion_info"] = {"hub": "", "port": ""}
+
+            if (port / "connector").exists():
+                # I think this is only USB-C
+                port_info["type_c"] = True
+                other_ports = [i for i in (port / "connector").glob("usb*-port*") if i.resolve() != port.resolve()]
+                assert len(other_ports) == 1
+                if (port / "peer").exists():
+                    assert port_info["companion_info"] == re.match(r"(?P<hub>usb\d+)-port(?P<port>\d+)", other_ports[0].resolve().name).groupdict()
+                port_info["companion_info"] = re.match(r"(?P<hub>usb\d+)-port(?P<port>\d+)", other_ports[0].resolve().name).groupdict()
+
+            if (port / "device").exists():
+                device = port / "device"
+                device_info = {
+                    # TODO: Use lsusb?
+                    "name": f"{quick_read_2(device, 'manufacturer')} {quick_read_2(device, 'product')}" if (device / "manufacturer").exists() else "Unknown Device",
+                    "speed": shared.USBDeviceSpeeds.from_speed(int(quick_read_2(device, "speed"))),
+                    "devices": [],  # I'm not dealing with this rn
+                }
+                port_info["devices"].append(device_info)
+
+            hub_info["ports"].append(port_info)
+        hub_info["ports"].sort(key=itemgetter("index"))
+        print(hub_info)
